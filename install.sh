6d5102a5328a7e85d2c0839baaf5e3f18f218c00
cze04 <smrckda1@fel.cvut.cz>
2022-01-25 11:02:03 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ MY_PATH=`(cd "$MY_PATH" && pwd)`
 
 echo "source $MY_PATH/scripts/shell_additions.sh" >> ~/.bashrc
 
-#sudo apt install -y vim net-tools moreutils python3-colcon-common-extensions
+sudo apt install -y vim net-tools moreutils python3-colcon-common-extensions
 
 ln -sf $MY_PATH/config/tmux.conf ~/.tmux.conf
 ln -sf $MY_PATH/tmux/tmux.sh ~/tmux.sh
