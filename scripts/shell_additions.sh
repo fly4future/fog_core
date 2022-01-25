b09a3c37a6f067eb196c743caaf554f6fdbcbbce
cze04 <smrckda1@fel.cvut.cz>
2022-01-25 10:40:02 +0200

    updated shell_additions, added colcon modifications

--- scripts/shell_additions.sh
+++ scripts/shell_additions.sh
@@ -1,7 +1,14 @@
+# colcon_cd setup
+source /usr/share/colcon_cd/function/colcon_cd.sh
+export _colcon_cd_root=/opt/ros/galactic/
+
+# aliases
 alias gs='git status'
 alias gd='git diff'
 alias sb='source ~/.bashrc'
-alias bt='colcon build --package-select $(basename `pwd`)'
+alias bt='colcon build --packages-select $(basename `pwd`)'
+
+## FUNCTIONS
 
 # #{ sourceShellDotfile()
 
@@ -87,6 +94,114 @@ killp() {
 
 # echo "killing $1"
 kill -9 "$1" > /dev/null 2> /dev/null
+}
+
+# #}
+
+# #{ colcon()
+
+colcon() {
+
+  CURRENT_PATH=`pwd`
+
+  case $* in
+
+    init*)
+
+      if [ ! -e "build/COLCON_IGNORE" ]; then # we are NOT at the workspace root
+        command colcon build # this creates a new workspace
+      fi
+
+      ;;
+
+    build*|b*)
+
+      # go up the folder tree until we find the build/COLCON_IGNORE file or until we reach the root
+      while [ ! -e "build/COLCON_IGNORE" ]; do
+        cd ..
+        if [[ `pwd` == "/" ]]; then
+          # we reached the root and didn't find the build/COLCON_IGNORE file - that's a fail!
+          echo "Cannot compile, probably not in a workspace (if you want to create a new workspace, call \"colcon init\" in its root first)".
+          return 1
+        fi
+      done
+
+      # if the flow got here, we found the build/COLCON_IGNORE file!
+      # this is the folder we're looking for - call the actual colcon command here
+      command colcon "$@" --symlink-install
+      ret=$? # remember the return value of the colcon command
+      cd "$CURRENT_PATH" # return to the path where this command was originaly called
+      return $ret # return the original return value of the colcon command
+
+      ;;
+
+    clean*)
+
+      if [ -e "build/COLCON_IGNORE" ]; then # we are at the workspace root
+        rm -r build install log
+        mkdir build
+        cd build
+        touch COLCON_IGNORE
+      else
+        while [ ! -e "build/COLCON_IGNORE" ]; do
+          cd ..
+
+          if [[ `pwd` == "/" ]]; then
+            echo "Cannot clean, not in a workspace!"
+            break
+          elif [ -e "build/COLCON_IGNORE" ]; then
+            rm -r build install log
+            mkdir build
+            cd build
+            touch COLCON_IGNORE
+            break
+          fi
+        done
+      fi
+
+      cd "$CURRENT_PATH" # return to the original folder where the command was called
+
+      ;;
+
+    *)
+      command colcon $@
+      ;;
+
+  esac
+}
+
+# #}
+
+# #{ cb()
+
+cb() {
+
+  # catkin?
+  PACKAGES=$(catkin list)
+  [ ! -z "$PACKAGES" ] && USE_CATKIN=1 || USE_CATKIN=0
+
+  # colcon?
+  CURRENT_PATH=`pwd`
+  while [ ! -e "build/COLCON_IGNORE" ]; do
+    cd ..
+
+    if [[ `pwd` == "/" ]]; then
+      break
+    fi
+  done
+  [[ `pwd` == "/" ]] && USE_COLCON=0
+  [ -e "build/COLCON_IGNORE" ] && USE_COLCON=1
+  cd "$CURRENT_PATH"
+
+  ret=1
+  [[ $USE_CATKIN == "1" ]] && [[ $USE_COLCON == "0" ]] && ( catkin build "$@"; ret=$? )
+  [[ $USE_CATKIN == "0" ]] && [[ $USE_COLCON == "1" ]] && ( colcon build "$@"; ret=$? )
+  [[ $USE_CATKIN == "1" ]] && [[ $USE_COLCON == "1" ]] && ( colcon build "$@"; ret=$? )
+  [[ $USE_CATKIN == "0" ]] && [[ $USE_COLCON == "0" ]] && echo "Cannot compile, not in a workspace"
+
+  unset USE_CATKIN
+  unset USE_COLCON
+  return $ret
 }
 
 # #}
