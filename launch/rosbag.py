63d4c93fbf5d18e759da020cf24ffbcf8b1e911c
daniel <smrckda1@fel.cvut.cz>
2022-03-18 14:47:59 +0100

    minor fix

--- launch/rosbag.py
+++ launch/rosbag.py
@@ -10,20 +10,27 @@ date = datetime.now().strftime('x_%Y_%m_%d__%H_%M_%S')
 PATH = HOME +"/bag_files/" + date
 
 # List of EXCLUDED topics:
-exclude=(
+exclude = ['(.*)octomap_server(.*)']
+
+# Examples:
+# exclude = []
+# exclude = ['(.*)octomap_server(.*)', '(.*)image_raw(.*)']
+
 ## Every topic containing "octomap"
-"(.*)octomap_server(.*)"
+#'(.*)octomap_server(.*)'
 ## Every topic containing "image_raw"
 #'(.*)image_raw(.*)'
 ## Every topic containing "theora"
 #'(.*)theora(.*)'
 ## Every topic containing "h264"
 ## '(.*)h264(.*)'
-)
+
 
 def launch_setup(context, *args, **kwargs):
     path = LaunchConfiguration("path").perform(context) + "/bag"
-    return [launch.actions.ExecuteProcess(cmd=['ros2', 'bag', 'record', '-a', '-o', path, '-x', exclude], output='screen')]
+    exclude_list = '|'.join(exclude)
+    print("excluding:", exclude_list)
+    return [launch.actions.ExecuteProcess(cmd=['ros2', 'bag', 'record', '-a', '-o', path, '-x', exclude_list], output='screen')]
 
 def generate_launch_description():
     ld = launch.LaunchDescription()
